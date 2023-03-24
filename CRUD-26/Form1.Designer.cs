63649f95c2e81076542342bb5e67ac9a72d84c2b
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-03-24 12:51:37 +0100

    Aggiungere i file di progetto.

---
+++ CRUD-26/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace CRUD_26
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.stampa = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.cerca = new System.Windows.Forms.Button();
+            this.modify = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pr = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // stampa
+            // 
+            this.stampa.Location = new System.Drawing.Point(89, 175);
+            this.stampa.Name = "stampa";
+            this.stampa.Size = new System.Drawing.Size(75, 23);
+            this.stampa.TabIndex = 0;
+            this.stampa.Text = "Salva";
+            this.stampa.UseVisualStyleBackColor = true;
+            this.stampa.Click += new System.EventHandler(this.stampa_Click);
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(205, 174);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(75, 23);
+            this.cancel.TabIndex = 1;
+            this.cancel.Text = "Cancella";
+            this.cancel.UseVisualStyleBackColor = true;
+            this.cancel.Click += new System.EventHandler(this.cancel_Click);
+            // 
+            // cerca
+            // 
+            this.cerca.Location = new System.Drawing.Point(329, 174);
+            this.cerca.Name = "cerca";
+            this.cerca.Size = new System.Drawing.Size(75, 23);
+            this.cerca.TabIndex = 2;
+            this.cerca.Text = "Ricerca";
+            this.cerca.UseVisualStyleBackColor = true;
+            this.cerca.Click += new System.EventHandler(this.cerca_Click);
+            // 
+            // modify
+            // 
+            this.modify.Location = new System.Drawing.Point(89, 254);
+            this.modify.Name = "modify";
+            this.modify.Size = new System.Drawing.Size(75, 23);
+            this.modify.TabIndex = 3;
+            this.modify.Text = "Modifica";
+            this.modify.UseVisualStyleBackColor = true;
+            this.modify.Click += new System.EventHandler(this.modify_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(282, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(205, 256);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(381, 257);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(205, 237);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nuovo Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(381, 236);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Nuovo Prezzo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nome";
+            // 
+            // pr
+            // 
+            this.pr.AutoSize = true;
+            this.pr.Location = new System.Drawing.Point(282, 60);
+            this.pr.Name = "pr";
+            this.pr.Size = new System.Drawing.Size(39, 13);
+            this.pr.TabIndex = 11;
+            this.pr.Text = "Prezzo";
+            // 
+            // listView1
+            // 
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(539, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(237, 416);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.pr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.modify);
+            this.Controls.Add(this.cerca);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.stampa);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button stampa;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button cerca;
+        private System.Windows.Forms.Button modify;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label pr;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ CRUD-26/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CRUD_26
+{
+    public partial class Form1 : Form
+    {
+        #region declare
+        string file;
+        public Form1()
+        {
+            InitializeComponent();
+            file = @"carrello.csv";
+            p = new Prodotto[100];
+            d = 0;
+        }
+        public struct Prodotto
+        {
+            public float prezzo;
+            public string nome;
+        }
+        public static Prodotto[] p;
+        public static int d;
+        
+        #endregion
+        #region pulsanti
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stampa_Click(object sender, EventArgs e)
+        {
+            p[d].nome = textBox1.Text;
+            p[d].prezzo = float.Parse(textBox2.Text);
+            d++;
+            vis();
+            
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cerca_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void modify_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+        #region funzioni
+        //funzioni per la stampa
+        public string proString(Prodotto p)
+        {
+            return "Nome: " + p.nome + " Prezzo: " + p.prezzo.ToString();
+        }
+        void vis()
+        {
+            listView1.Items.Clear();
+            for (int i = 0; i < d; i++)
+            {
+                listView1.Items.Add(proString(p[i]));
+            }
+        }
+        //funzioni per la ricerca
+        int search(Prodotto[] p, string nome)
+        {
+            int pos;
+            for (int i = 0; i < d; i++)
+            {
+                if (p[i].nome == nome)
+                {
+                    pos = i;
+                    return pos;
+                }
+            }
+            pos = -1;
+            return pos;
+
+        }
+        //funzioni per la modifica
+
+        //funzioni per la cancellazione
+        void cancel(Prodotto p)
+        {
+            using (StreamReader st = File.OpenText(file))
+            {
+                
+                while ((p.nome= st.ReadLine()) != null)
+                {
+                    
+                }
+            }
+        }
+        #endregion
+    }
+}
